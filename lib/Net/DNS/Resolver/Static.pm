f78028a7ed8fa0d3c16192645f7557f8d2bcd055
Jonathan Hall <jhall@efolder.net>
2014-09-17 14:51:38 -0500

    Add basic functionality and tests.

--- lib/Net/DNS/Resolver/Static.pm
+++ lib/Net/DNS/Resolver/Static.pm
@@ -1,60 +1,137 @@
 package Net::DNS::Resolver::Static;
 
-use 5.014002;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Net::DNS::Resolver::Static ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
+use base 'Net::DNS::Resolver';
+use Net::IP;
 
 our $VERSION = '0.01';
 
+sub new {
+    my ( $class, %args ) = @_;
+    my $static_data = delete $args{static_data};
+    my $static_file = delete $args{static_file};
 
-# Preloaded methods go here.
+    my $self = $class->SUPER::new( %args );
+
+    my $package = __PACKAGE__;
+
+    if ( $static_file ) {
+        open my $F, '<', $static_file
+            or die "$package: FATAL: Unable to open '$static_file': $!\n";
+        local $/;
+        my $data = <$F>;
+        close $F;
+        eval { $self->_populate_static_cache($data) };
+        if ( $@ ) {
+            die "$package: FATAL: Error reading '$static_file': $@";
+        }
+    }
+    if ( $static_data ) {
+        eval { $self->_populate_static_cache($static_data) };
+        if ( $@ ) {
+            die "$package: FATAL: $@";
+        }
+    }
+    if ( ! $self->{static_dns} ) {
+        die "$package: FATAL: No static cache provided";
+    }
+    return $self;
+}
+
+sub _populate_static_cache {
+    my ( $self, $data ) = @_;
+    my $line_num = 0;
+    for my $line (split /\n/,$data) {
+        $line_num++;
+        $line =~ s/;.*//;
+        $line =~ s/^\s+//;
+        $line =~ s/\s+$//;
+        next if $line =~ /^$/;
+        my ( $host, $class, $type, $value )
+            = $line =~ /^\s*([\/_a-z\.0-9-]+)\s+(?:\d+\s+)?([A-Z]+)\s+([A-Z]+)\b\s*([^\s].*)?/;
+        if ( ! $host ) {
+            die "Cache syntax error, $line_num: $line\n";
+        }
+        $line = $value if defined $value and $value =~ /^error\s/;
+        if ( $host !~ /\.$/ ) {
+            $host .= '.';
+        }
+        push @{ $self->{static_dns}{"$host $class $type"} ||= [] }, ( $value ? $line : undef );
+    }
+}
+
+sub send {
+    my ( $self, $host, $type, $class ) = @_;
+    $class ||= 'IN';
+    if ( ! $type ) {
+        if ( Net::IP::ip_is_ipv4($host) or Net::IP::ip_is_ipv6($host) ) {
+            $type = 'PTR';
+            $host = Net::IP::ip_reverse($host);
+        } else {
+            $type = 'A';
+        }
+    }
+    if ( $host !~ /\.$/ ) {
+        $host .= '.';
+    }
+    if ( ! exists $self->{static_dns}{"$host $class $type"} ) {
+        if ( $type eq 'CNAME' or ! exists $self->{static_dns}{"$host $class CNAME"} ) {
+            $self->_cache_miss($host,$class,$type)
+        }
+        $type = 'CNAME';
+    }
+    my $cached_record = $self->{static_dns}{"$host $class $type"};
+    if ( defined $cached_record->[0] and $cached_record->[0] =~ /^error\s+"(.*)"$/ ) {
+        $self->{errorstring} = $1;
+        return;
+    }
+    my $answer = Net::DNS::Packet->new($host,$type,$class);
+    if ( defined $cached_record ) {
+        $answer->push( answer =>
+            map { Net::DNS::RR->new( $_ ) }
+            grep { $_ }
+            @$cached_record );
+    }
+    return $answer;
+}
+
+sub _cache_miss {
+    my ( $self, $host, $class, $type ) = @_;
+    my $package = __PACKAGE__;
+    warn <<EOF;
+!!!! $package !!!!
+No cached answer available for '$host $class $type'
+EOF
+    die "Dying\n";  # Net::DNS::Async captures $@, so we don't see the 'die' output
+                    # in some caess, so we display all the useful info in the warning
+                    # instead
+}
+
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Net::DNS::Resolver::Static - Perl extension for blah blah blah
+Net::DNS::Resolver::Static - Static DNS resolver class
 
 =head1 SYNOPSIS
 
-  use Net::DNS::Resolver::Static;
-  blah blah blah
+  use Net::DNS::Resolver::Static './static-dns-cache.txt';
+
+  my $res = Net::DNS::Resolver::Static->new;
+
+  # Then use it just like Net::DNS::Resolver
+
+  my $answer = $res->query('example.com','MX');
 
 =head1 DESCRIPTION
 
-Stub documentation for Net::DNS::Resolver::Static, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
+This is a subclass of L<Net::DNS::Resolver> which reads all DNS answers
+from a local static file, rather than from the network. It is designed
+for the purpose of unit testing code which uses Net::DNS::Resolver.
 
 
 =head1 SEE ALSO
@@ -79,6 +156,5 @@ Copyright (C) 2014 by Jonathan Hall
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
--- t/Net-DNS-Resolver-Static.t
+++ t/Net-DNS-Resolver-Static.t
@@ -1,18 +1,77 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl Net-DNS-Resolver-Static.t'
-
-#########################
-
-# change 'tests => 1' to 'tests => last_test_to_print';
-
+#!/usr/bin/perl
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+#use Test::More tests => 11;
+use Test::More 'no_plan';
+use Test::Exception;
+
 BEGIN { use_ok('Net::DNS::Resolver::Static') };
 
-#########################
+throws_ok { Net::DNS::Resolver::Static->new() } qr/No static cache provided/;
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+{
+    my $res = bless {}, 'Net::DNS::Resolver::Static';
 
+    lives_ok { $res->_populate_static_cache('') };
+    dies_ok { $res->_populate_static_cache('bogus data') } qr/Cache syntax error/;
+}
+
+{
+    # Test that string parsing works
+    my $res;
+
+    ok( $res = Net::DNS::Resolver::Static->new(
+            static_data => '
+a.com.  3600    IN      A   1.2.3.4
+b.com   IN  A   1.2.3.4'
+        ));
+    is_deeply( $res->{static_dns}, {
+        'b.com. IN A' => ['b.com   IN  A   1.2.3.4'],
+        'a.com. IN A' => ['a.com.  3600    IN      A   1.2.3.4']
+    } )
+};
+
+{
+    # Test that file parsing works
+    my $res;
+
+    ok( $res = Net::DNS::Resolver::Static->new( static_file => './t/dns-cache.txt' ));
+    is_deeply( $res->{static_dns}, {
+        'y.com. IN A' => [ undef ],
+        'z.com. IN A' => [ 'z.com.  3600    IN  A   5.4.3.2' ],
+        'x.com. IN A' => [ 'error "Foo error"' ]
+    } );
+};
+
+{
+    my $res = Net::DNS::Resolver::Static->new( static_data => '
+a.com.  IN  A   1.2.3.4
+');
+    ok( $res->send('a.com'), 'Test that trailing . is added for lookup');
+    my $packet;
+    ok( $packet = $res->send('a.com.'), 'Basic lookup returns an answer');
+    is( scalar $packet->answer, 1, 'a.com. has one answer' );
+    my ( $rr ) = $packet->answer;
+    is( $rr->ttl, 0, 'a.com. IN A TTL as expected');
+    is( $rr->type, 'A', 'a.com. IN A type as expected' );
+    is( $rr->class, 'IN', 'a.com. IN A class as expected' );
+    is( $rr->address, '1.2.3.4', 'a.com. IN A address as expected' );
+};
+
+{
+    my $res = Net::DNS::Resolver::Static->new( static_data => '
+b.com.  IN  A   error "Foo error"
+');
+    ok( ! $res->send('b.com'), 'Test that error is cached' );
+    is( $res->errorstring, 'Foo error', 'Error message as expected' );
+}
+
+{
+    my $res = Net::DNS::Resolver::Static->new( static_data => '
+c.com.  IN  A   ; No record
+');
+    my $packet;
+    ok( $packet = $res->send('c.com'), 'Test for negative result' );
+    is( scalar $packet->answer, 0, 'No results for c.com. IN A');
+}
